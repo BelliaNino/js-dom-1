b48a962df30988991671ea8d36efd48897527eab
BelliaNino <bellianino1996@gmail.com>
2026-02-11 17:23:04 +0100

    add exercise lamp on/off

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ const elLamp = document.getElementById (`lamp`)
 const elOnBtn = document.getElementById (`onBtn`)
 const elOffBtn = document.getElementById (`offBtn`)
 
-console.log(elLamp);
+//Richiamando il bottone "on", con "click" cambio l'immagine 
+elOffBtn.addEventListener(`click`, function(){
+    elLamp.src = "./img 4/white_lamp.png"
+})
+
+//Eseguo la stessa operazione ma al contrario 
+elOnBtn.addEventListener(`click`, function(){
+    elLamp.src = "./img 4/yellow_lamp.png"
+})
+
+
+
+
